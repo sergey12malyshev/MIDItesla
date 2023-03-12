0a343c37ea6af0114727b7cf05c571b91efc3507
sergey12malyshev <59499846+sergey12malyshev@users.noreply.github.com>
2023-03-12 22:01:13 +0500

    Рефакторинг

--- lcd1602.py
+++ lcd1602.py
@@ -1,5 +1,6 @@
 #  MBTechWorks.com 2016
 #  Control an LCD 1602 display from Raspberry Pi with Python programming
+# Script update 2022, Malyshev S.E.
 
 #!/usr/bin/python
 
@@ -28,7 +29,6 @@ import datetime #  m.s for system data and time
 import sys, select, termios, os
 from gpiozero import LoadAverage,CPUTemperature
 
-
 # GPIO to LCD mapping
 LCD_RS = 7   # Pi pin 26
 LCD_E  = 8   # Pi pin 24
@@ -44,142 +44,126 @@ LCD_CHARS = 16    # Characters per line (16 max)
 LCD_LINE_1 = 0x80 # LCD memory location for 1st line
 LCD_LINE_2 = 0xC0 # LCD memory location 2nd line
 
-BUZZ = 17
 
-# Define main program code
 def main():
-  
-  GPIO.setwarnings(False)
-  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
-  GPIO.setup(LCD_E, GPIO.OUT)  # Set GPIO's to output mode
-  GPIO.setup(LCD_RS, GPIO.OUT)
-  GPIO.setup(LCD_D4, GPIO.OUT)
-  GPIO.setup(LCD_D5, GPIO.OUT)
-  GPIO.setup(LCD_D6, GPIO.OUT)
-  GPIO.setup(LCD_D7, GPIO.OUT)
-  
-  GPIO.setup(BUZZ,GPIO.OUT)
-  GPIO.output(BUZZ, False) 
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+    GPIO.setup(LCD_E, GPIO.OUT)  # Set GPIO's to output mode
+    GPIO.setup(LCD_RS, GPIO.OUT)
+    GPIO.setup(LCD_D4, GPIO.OUT)
+    GPIO.setup(LCD_D5, GPIO.OUT)
+    GPIO.setup(LCD_D6, GPIO.OUT)
+    GPIO.setup(LCD_D7, GPIO.OUT)
 
-# Initialize display
-  lcd_init()
+    # Initialize display
+    lcd_init()
 
+    old_minute = 0 
+    load_old = 0
+    cpu_str_old = 0
+  
+    while True:   
+        time.sleep(0.3)
+        #and the access its now method simpler
+        now = datetime.datetime.now()
+        str_now = str(now)
+
+        load = "CPU:"+str(int(LoadAverage(minutes=1).load_average*12))+"%"
+        cpu = CPUTemperature(min_temp=50, max_temp=90)
+        cpu_str = round(cpu.temperature,1)
+        cpu_str = str(cpu_str)+"C"
     
-# Loop - send text and sleep 3 seconds between texts
-# Change text to anything you wish, but must be 16 characters or less
-  old_minute = 0 
-  load_old = 0
-  cpu_str_old = 0
-  
-  while True:
+        #str_hour= str(now.hour) 
+        #str_minute= str(now.minute)
+        #str_second= str(now.second)
+        #str_sum = "str_hour" + "str_minute" + "str_second"
+    
+        print('Initial temperature: {}C'.format(cpu.temperature))
+    
+        minute=(now.minute)
+        if (load_old != load)|(old_minute != minute)|(cpu_str_old != cpu_str):
+           load_old = load 
+           old_minute = minute
+           cpu_str_old = cpu_str
       
-    time.sleep(0.3)
-#and the access its now method simpler
-    now = datetime.datetime.now()
-    str_now = str(now)
-
-    load = "CPU:"+str(int(LoadAverage(minutes=1).load_average*12))+"%"
-    cpu = CPUTemperature(min_temp=50, max_temp=90)
-    cpu_str = round(cpu.temperature,1)
-    cpu_str = str(cpu_str)+"C"
-    
-    #str_hour= str(now.hour) 
-    #str_minute= str(now.minute)
-    #str_second= str(now.second)
-    #str_sum = "str_hour" + "str_minute" + "str_second"
-    
-    print('Initial temperature: {}C'.format(cpu.temperature))
-    
-    minute=(now.minute)
-    if (load_old != load)|(old_minute != minute)|(cpu_str_old != cpu_str):
-      load_old = load 
-      old_minute = minute
-      cpu_str_old = cpu_str
-      
-      lcd_text(load +"  t:"+ cpu_str,LCD_LINE_1)
-      lcd_text(str_now ,LCD_LINE_2)
-      print("now")
+        lcd_text(load +"  t:"+ cpu_str,LCD_LINE_1)
+        lcd_text(str_now ,LCD_LINE_2)
+        print("now")
  
-# End of main program code
-
-
 # Initialize and clear display
 def lcd_init():
-  lcd_write(0x33,LCD_CMD) # Initialize
-  lcd_write(0x32,LCD_CMD) # Set to 4-bit mode
-  lcd_write(0x06,LCD_CMD) # Cursor move direction
-  lcd_write(0x0C,LCD_CMD) # Turn cursor off
-  lcd_write(0x28,LCD_CMD) # 2 line display
-  lcd_write(0x01,LCD_CMD) # Clear display
-  time.sleep(0.0005)     # Delay to allow commands to process
+    lcd_write(0x33,LCD_CMD)  # Initialize
+    lcd_write(0x32,LCD_CMD)  # Set to 4-bit mode
+    lcd_write(0x06,LCD_CMD)  # Cursor move direction
+    lcd_write(0x0C,LCD_CMD)  # Turn cursor off
+    lcd_write(0x28,LCD_CMD)  # 2 line display
+    lcd_write(0x01,LCD_CMD)  # Clear display
+    time.sleep(0.0005)       # Delay to allow commands to process
 
 def lcd_write(bits, mode):
-# High bits
-  GPIO.output(LCD_RS, mode) # RS
+    # High bits
+    GPIO.output(LCD_RS, mode) # RS
 
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x10==0x10:
-    GPIO.output(LCD_D4, True)
-  if bits&0x20==0x20:
-    GPIO.output(LCD_D5, True)
-  if bits&0x40==0x40:
-    GPIO.output(LCD_D6, True)
-  if bits&0x80==0x80:
-    GPIO.output(LCD_D7, True)
+    GPIO.output(LCD_D4, False)
+    GPIO.output(LCD_D5, False)
+    GPIO.output(LCD_D6, False)
+    GPIO.output(LCD_D7, False)
+    if bits&0x10 == 0x10:
+        GPIO.output(LCD_D4, True)
+    if bits&0x20 == 0x20:
+       GPIO.output(LCD_D5, True)
+    if bits&0x40 == 0x40:
+       GPIO.output(LCD_D6, True)
+    if bits&0x80 == 0x80:
+       GPIO.output(LCD_D7, True)
 
 # Toggle 'Enable' pin
-  lcd_toggle_enable()
+    lcd_toggle_enable()
 
 # Low bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x01==0x01:
-    GPIO.output(LCD_D4, True)
-  if bits&0x02==0x02:
-    GPIO.output(LCD_D5, True)
-  if bits&0x04==0x04:
-    GPIO.output(LCD_D6, True)
-  if bits&0x08==0x08:
-    GPIO.output(LCD_D7, True)
-
-# Toggle 'Enable' pin
-  lcd_toggle_enable()
+    GPIO.output(LCD_D4, False)
+    GPIO.output(LCD_D5, False)
+    GPIO.output(LCD_D6, False)
+    GPIO.output(LCD_D7, False)
+    if bits&0x01 == 0x01:
+        GPIO.output(LCD_D4, True)
+    if bits&0x02 == 0x02:
+        GPIO.output(LCD_D5, True)
+    if bits&0x04 == 0x04:
+        GPIO.output(LCD_D6, True)
+    if bits&0x08 == 0x08:
+        GPIO.output(LCD_D7, True)
+             
+    lcd_toggle_enable() # Toggle 'Enable' pin
 
 def lcd_toggle_enable():
-  time.sleep(0.0005)
-  GPIO.output(LCD_E, True)
-  time.sleep(0.0005)
-  GPIO.output(LCD_E, False)
-  time.sleep(0.0005)
+    time.sleep(0.0005)
+    GPIO.output(LCD_E, True)
+    time.sleep(0.0005)
+    GPIO.output(LCD_E, False)
+    time.sleep(0.0005)
 
 def lcd_text(message,line):
-  # Send text to display
-  message = message.ljust(LCD_CHARS," ")
+    # Send text to display
+    message = message.ljust(LCD_CHARS," ")
+    lcd_write(line, LCD_CMD)
 
-  lcd_write(line, LCD_CMD)
-
-  for i in range(LCD_CHARS):
-    lcd_write(ord(message[i]),LCD_CHR)
+    for i in range(LCD_CHARS):
+        lcd_write(ord(message[i]),LCD_CHR)
 
 
-#Begin program
+# Begin program
 try:
-  main()
+    main()
 	  
 except KeyboardInterrupt:
-  pass
+    pass
 	
 finally:
-  GPIO.output(BUZZ, True) 
-  lcd_write(0x01, LCD_CMD)
-  lcd_text("System Exit!",LCD_LINE_1)
-  lcd_text("bagube",LCD_LINE_2)
-  time.sleep(0.1)
-  GPIO.cleanup()
-  #os.system("sudo reboot")
-  os.system("poweroff")
+    lcd_write(0x01, LCD_CMD)
+    lcd_text("System Exit!", LCD_LINE_1)
+    lcd_text("Bagube", LCD_LINE_2)
+    time.sleep(0.1)
+    GPIO.cleanup()
+    #os.system("sudo reboot")
+    #os.system("poweroff")
